0ffb578501c6adc1434e2876a524e9e4d94ee1a9
XintongTheCoder <xintongmi@gmail.com>
2023-07-03 10:07:26 -0700

    refactor fetch data functions of dog board page

--- src/app/dog-board/page.tsx
+++ src/app/dog-board/page.tsx
@@ -26,43 +26,41 @@ export default function DogBoard(): ReactElement {
   const dogBoard = useAppSelector((state) => state.dogBoard);
   const dispatch = useAppDispatch();
 
-  const fetchDogs = async () => {
-    try {
-      const dogSearchResp = await client.get<DogSearchResp>('/dogs/search', {
-        params: {
-          breeds: dogBoard.selectedBreeds,
-          zipCodes: dogBoard.selectedZipCodes,
-          ageMin: dogBoard.ageMin,
-          ageMax: dogBoard.ageMax,
-        },
-      });
-      const dogsResp = await client.post<DogsResp[]>('/dogs', dogSearchResp.data.resultIds);
-      const conformedDogs = dogsResp.data.map((dog) => {
-        // eslint-disable-next-line camelcase
-        const { zip_code, ...rest } = dog;
-        // eslint-disable-next-line camelcase
-        return { zipCode: zip_code, ...rest };
-      });
-      dispatch(updateDogs(conformedDogs));
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
   useEffect(() => {
-    async function fetchBreeds() {
+    const fetchBreeds = async () => {
       try {
         const breedsResp = await client.get<string[]>('/dogs/breeds');
         dispatch(updateBreeds(breedsResp.data));
-        fetchDogs();
       } catch (err) {
         console.error(err);
       }
-    }
+    };
     fetchBreeds();
   }, [dispatch]);
 
   useEffect(() => {
+    const fetchDogs = async () => {
+      try {
+        const dogSearchResp = await client.get<DogSearchResp>('/dogs/search', {
+          params: {
+            breeds: dogBoard.selectedBreeds,
+            zipCodes: dogBoard.selectedZipCodes,
+            ageMin: dogBoard.ageMin,
+            ageMax: dogBoard.ageMax,
+          },
+        });
+        const dogsResp = await client.post<DogsResp[]>('/dogs', dogSearchResp.data.resultIds);
+        const conformedDogs = dogsResp.data.map((dog) => {
+          // eslint-disable-next-line camelcase
+          const { zip_code, ...rest } = dog;
+          // eslint-disable-next-line camelcase
+          return { zipCode: zip_code, ...rest };
+        });
+        dispatch(updateDogs(conformedDogs));
+      } catch (err) {
+        console.error(err);
+      }
+    };
     fetchDogs();
   }, [
     dogBoard.selectedBreeds,
